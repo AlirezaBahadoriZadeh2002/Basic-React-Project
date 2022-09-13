b6183c5bd0a945c9549c1e94ac42f304eb38e9e6
AlirezaBahadoriZadeh2002 <alireza.bahadori.zadeh.2002@gmail.com>
2022-09-13 04:53:56 +0430

    Edit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Happy React App!!!
+	  Alireza
         </p>
         <a
           className="App-link"
